738d81fb5a154acfcbcffafa7a0267a3a32e531d
ta-nett@mail.ru <klmnopgg>
2022-10-17 12:08:27 +0300

    add PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const history = require('express-history-api-fallback')
 
 const app = express()
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname + '/dist')))
 app.use(history('index.html', { root: './dist' }));
