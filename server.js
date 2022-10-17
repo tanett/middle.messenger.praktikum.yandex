bbc685602504b67047eea966bbc0e4c843735155
ta-nett@mail.ru <klmnopgg>
2022-10-17 11:28:11 +0300

    add fix serv

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const path = require('path')
 const history = require('express-history-api-fallback')
 
 const app = express()
-const PORT = process.env. port || 3000
+const PORT = process.env.port || 3000
 
 app.use(express.static(path.join(__dirname + '/dist')))
 app.use(history('index.html', { root: './dist' }));
+app.use('*', (req, res)=>{
+  res.status(200).sendFile(__dirname + '/dist/index.html')
+})
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`)
